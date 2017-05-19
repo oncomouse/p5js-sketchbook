522e93ea9e1b1a7e1c92575a143b242c71965ace
oncomouse <onocmouse@gmail.com>
2017-05-19 08:55:30 -0500

    Initial Commit

--- app/utilities/color.js
+++
@@ -1,7 +0,0 @@
-export const randomColor = () => color( random(0,255), random(0,255), random(0,255) );
-export const lighten = (c, percentage) => color(
-	red(c),
-	green(c),
-	blue(c),
-	map(percentage, 0, 1, 0, 255)
-);
